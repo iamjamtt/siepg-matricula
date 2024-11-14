0b9931807d1d2e6185385e9edaae29f58484bdde
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-11-14 00:06:44 -0500

    feat: Agregar componente de título de página y actualizar la vista de inicio

--- resources/views/livewire/inicio/index.blade.php
+++ resources/views/livewire/inicio/index.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <h1>Inicio</h1>
+<div class="row">
+    <div class="col-12">
+        <x-page-title title="Inicio">
+            <x-page-title.item active>
+                Inicio
+            </x-page-title.item>
+        </x-page-title>
+    </div>
+    <div class="col-12">
+        <div class="row">
+            <div class="col-md-12">
+                //
+            </div>
+        </div>
+    </div>
 </div>

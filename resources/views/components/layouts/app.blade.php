27753c75197ec06eb5db0683c564c3ed4fda3506
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-11-13 22:20:22 -0500

    feat: Actualizar el tema y modificar el menú de navegación en la barra lateral

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,7 @@
     data-sidebar-size="lg"
     data-sidebar-image="none"
     data-preloader="disable"
-    data-theme="vintage"
+    data-theme="minimal"
     data-theme-colors="blue"
     data-sidebar-visibility="show"
     data-layout-style="default"
@@ -186,118 +186,56 @@
                         <li class="menu-title">
                             <span data-key="t-menu">Menu</span>
                         </li>
+
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse"
-                                role="button" aria-expanded="false" aria-controls="sidebarDashboards">
-                                <i class="ri-dashboard-2-line"></i>
-                                <span data-key="t-dashboards">Dashboards</span>
+                            <a
+                                href="{{ route('inicio.index') }}"
+                                class="nav-link menu-link {{ request()->routeIs('inicio.index') ? 'active' : '' }}"
+                            >
+                                <i class="ri-home-6-line"></i>
+                                <span>Inicio</span>
                             </a>
-                            <div class="collapse menu-dropdown" id="sidebarDashboards">
-                                <ul class="nav nav-sm flex-column">
-                                    <li class="nav-item">
-                                        <a href="dashboard-analytics.html" class="nav-link" data-key="t-analytics">
-                                            Analytics
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="dashboard-crm.html" class="nav-link" data-key="t-crm">
-                                            CRM
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="index.html" class="nav-link" data-key="t-ecommerce">
-                                            Ecommerce
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="dashboard-blog.html" class="nav-link"><span
-                                                data-key="t-blog">Blog</span>
-                                            <span class="badge bg-success" data-key="t-new">New</span></a>
-                                    </li>
-                                </ul>
-                            </div>
                         </li>
-                        <!-- end Dashboard Menu -->
 
                         <li class="menu-title">
                             <i class="ri-more-fill"></i>
-                            <span data-key="t-pages">Pages</span>
+                            <span>Otros</span>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="widgets.html">
-                                <i class="ri-honour-line"></i>
-                                <span data-key="t-widgets">Widgets</span>
+                            <a
+                                href="#"
+                                class="nav-link menu-link"
+                            >
+                                <i class="ri-folder-5-line"></i>
+                                <span>Matriculas</span>
                             </a>
                         </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link menu-link" href="#sidebarForms" data-bs-toggle="collapse"
-                                role="button" aria-expanded="false" aria-controls="sidebarForms">
+                        {{-- <li class="nav-item">
+                            <a
+                                class="nav-link menu-link"
+                                href="#sidebarForms" data-bs-toggle="collapse"
+                                role="button" aria-expanded="false" aria-controls="sidebarForms"
+                            >
                                 <i class="ri-file-list-3-line"></i>
-                                <span data-key="t-forms">Forms</span>
+                                <span>Forms</span>
                             </a>
                             <div class="collapse menu-dropdown" id="sidebarForms">
                                 <ul class="nav nav-sm flex-column">
                                     <li class="nav-item">
-                                        <a href="forms-elements.html" class="nav-link"
-                                            data-key="t-basic-elements">Basic Elements</a>
+                                        <a href="#" class="nav-link">
+                                            Basic Elements
+                                        </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a href="forms-select.html" class="nav-link" data-key="t-form-select">
+                                        <a href="#" class="nav-link">
                                             Form Select
                                         </a>
                                     </li>
-                                    <li class="nav-item">
-                                        <a href="forms-checkboxs-radios.html" class="nav-link"
-                                            data-key="t-checkboxs-radios">Checkboxs & Radios</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="forms-pickers.html" class="nav-link" data-key="t-pickers">
-                                            Pickers
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="forms-masks.html" class="nav-link" data-key="t-input-masks">Input
-                                            Masks</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="forms-advanced.html" class="nav-link"
-                                            data-key="t-advanced">Advanced</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="forms-range-sliders.html" class="nav-link"
-                                            data-key="t-range-slider">
-                                            Range Slider
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="forms-validation.html" class="nav-link"
-                                            data-key="t-validation">Validation</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="forms-wizard.html" class="nav-link"
-                                            data-key="t-wizard">Wizard</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="forms-editors.html" class="nav-link"
-                                            data-key="t-editors">Editors</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="forms-file-uploads.html" class="nav-link"
-                                            data-key="t-file-uploads">File Uploads</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="forms-layouts.html" class="nav-link"
-                                            data-key="t-form-layouts">Form Layouts</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="forms-select2.html" class="nav-link"
-                                            data-key="t-select2">Select2</a>
-                                    </li>
                                 </ul>
                             </div>
-                        </li>
+                        </li> --}}
                     </ul>
                 </div>
                 <!-- Sidebar -->

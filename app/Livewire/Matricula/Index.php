3037d01ca365e3ade62fb5a6cf57da6a9f3537b4
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-11-16 21:49:55 -0500

    feat: Agregar vista de matrícula con layout y título personalizados

--- app/Livewire/Matricula/Index.php
+++ app/Livewire/Matricula/Index.php
@@ -2,10 +2,14 @@
 
 namespace App\Livewire\Matricula;
 
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Layout('components.layouts.app')]
 class Index extends Component
 {
+    #[Title('Mis Matriculas | SIEPG Matricula')]
     public function render()
     {
         return view('livewire.matricula.index');
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Livewire\Auth\Login as AuthLogin;
 use App\Livewire\Inicio\Index as InicioIndex;
+use App\Livewire\Matricula\Index as MatriculaIndex;
 
 Route::get('/login', AuthLogin::class)
     ->middleware('guest')
@@ -12,5 +13,9 @@ Route::get('/login', AuthLogin::class)
 Route::get('/', InicioIndex::class)
     ->middleware('auth')
     ->name('inicio.index');
+
+Route::get('/matricula', MatriculaIndex::class)
+    ->middleware('auth')
+    ->name('matricula.index');
 
 //
